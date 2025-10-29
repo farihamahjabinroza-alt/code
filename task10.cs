261ba11b1f5aa318f6e869d740b2d0e41443c5c3
Fariha Mahjabin Roza <farihamahjabinroza@gmail.com>
2025-10-29 10:45:19 +0600

    Update task10.cs

--- task10.cs
+++ task10.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace project
 {
     internal class task10
@@ -12,16 +11,12 @@ namespace project
         {
             Console.Write("Enter first number: ");
             double a = double.Parse(Console.ReadLine());
-
             Console.Write("Enter second number: ");
             double b = double.Parse(Console.ReadLine());
-
-
             bool result = (a > 0 && a < 1) && (b > 0 && b < 1);
-
             Console.WriteLine(result);
-
             Console.ReadLine();
         }
     }
 }
+
